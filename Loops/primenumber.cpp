f26d13dabe14280222454a73235859e41b518f58
aditya9277 <adisamarth20@gmail.com>
2023-05-07 21:48:32 +0530

    added code

--- Loops/primenumber.cpp
+++ Loops/primenumber.cpp
@@ -5,5 +5,16 @@ int main(){
     int n,i,count=0;
     cout<<"enter n : ";
     cin>>n;
-    
+    for(i=1;i<=n;i++){
+        if(n%i==0){
+            count++;
+        }
+
+    }
+    if(count==2){
+        cout<<"Prime Number";
+    }
+    else
+        cout<<"Non Prime Number";
+    return 0;
 }
